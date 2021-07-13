1d3afddb9ef2fbdc056820c1d3fcddbb73fc0311
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-07-13 12:48:04 +0530

    using card widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,88 +9,79 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.only(top: 200),
-            child: Column(
-            children: <Widget>[
-             CircleAvatar(
-              radius: 50.0,
-             backgroundImage: AssetImage('images/pic1.png'),
-            ),
-
-            Text(
-            'Abdul Ragib',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Pacifico',
-              ),
-             ),
-
-             Text(
-            'Flutter Developer',
-            style: TextStyle(
-              fontSize: 17,
-              color: Colors.teal[100],
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Source Sans Pro',
-              ),
-             ),
-             
-             Padding(padding: EdgeInsets.all(5)),
-             Card(
-              child: Container(
-                 padding: EdgeInsets.all(6),
-                 color: Colors.white,
-                 width: 300,
-                 height:35,
-                child: Row(
-                   children: <Widget>[
-                     Icon(Icons.call,
-                     color: Colors.teal),
-                     SizedBox(width:10),
-                     Text("+91 7033037325",
-                     style: TextStyle(
-                       color: Colors.teal.shade900,
-                       fontSize: 16,
-                       fontFamily: 'Source Sans Pro'
-                     ),
-                    ),
-                   ],
-                 ),
-               ),
-             ),
-             
-             SizedBox(height: 8),
-              Card(
-                child: Container(
-                  padding: EdgeInsets.all(6),
-                 color: Colors.white,
-                 width: 300,
-                 height:35,
-                child: Row(
-                   children: <Widget>[
-                     Icon(Icons.email,
-                     color: Colors.teal),
-                     SizedBox(width:10),
-                     Text("abdulragib36@gmail.com",
-                     style: TextStyle(
-                       color: Colors.teal.shade900,
-                       fontSize: 16,
-                       fontFamily: 'Source Sans Pro'
-                     ),
-                    ),
-                   ],
-                 ),
-             ),
-              ),
-            ],
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           CircleAvatar(
+            radius: 50.0,
+           backgroundImage: AssetImage('images/pic1.png'),
            ),
-          ),
+
+          Text(
+          'Abdul Ragib',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Pacifico',
+            ),
+           ),
+
+           Text(
+          'Flutter Developer',
+          style: TextStyle(
+            fontSize: 17,
+            color: Colors.teal[100],
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Source Sans Pro',
+            ),
+           ),
+
+           SizedBox(
+           height: 10.0, 
+           width:150,
+           child: Divider(color: Colors.teal.shade100),
+           ),
+
+           Padding(padding: EdgeInsets.all(5)),
+           Card(
+             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.call,
+                 color: Colors.teal),
+                 title: Text("+91 7033037325",
+                 style: TextStyle(
+                   color: Colors.teal.shade900,
+                   fontSize: 18,
+                   fontFamily: 'Source Sans Pro'
+                 ),
+                ),
+            )
+           ),
+           
+           
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.email,
+                color: Colors.teal),
+
+                title: Text("abdulragib36@gmail.com",
+                   style: TextStyle(
+                      color: Colors.teal.shade900,
+                     fontSize: 18,
+                    fontFamily: 'Source Sans Pro'
+                    ),
+                  ),
+                
+              ),
+            ),
+          ],
+         ),
         ),
       ),
     );
   }
 }
+
